61c6d2680b02cd0751efd05456d34faa01cb3e98
ZeynelaErtur <zeynelabidin.ertur@ideasoft.com.tr>
2019-11-27 16:27:14 +0300

    second step: tutorial bitti. Dbye kaydetme dbden veri çekme işlemleri yapıldı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,17 @@ import { Camera } from '@ionic-native/camera/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot()
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
+import { Component, OnInit} from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -8,18 +8,9 @@ import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
 })
 export class Tab2Page {
   currentImage: any;
-  constructor(private camera: Camera) { }
-  takePicture() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    };
-    this.camera.getPicture(options).then((imageData) => {
-      this.currentImage = 'data:image/jpeg;base64' + imageData;
-    }, (err) => {
-      console.log('Camera issue: ' + err);
-    });
+  constructor(public photoService: PhotoService) { }
+  ngOnInıt() {
+    this.photoService.loadSaved();
   }
 }
+
